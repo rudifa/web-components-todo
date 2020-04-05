f758a9d0cdd1ff3047c3c3ad743a671112bc40f9
Rudi Farkas <rudi.farkas@gmail.com>
2020-04-05 20:24:16 +0200

    finish adding 'todo-list', TodoList, not yet in use

--- src/WcTodo.js
+++ src/WcTodo.js
@@ -6,8 +6,40 @@ import { html, css, LitElement } from 'lit-element';
 class TodoList extends LitElement {
   static get properties() {
     return {
-      todos: { type: Array }
+      todos: { type: Array },
+    };
+  }
+
+  render() {
+    if (!this.todos) {
+      return html``;
     }
+
+    return html`
+      <ol>
+      ${this.todos.map(
+        todo => html`
+          <li>
+            <input
+              type="checkbox"
+              .checked=${e => this._changeTodoFinished(e, todo)}
+            />
+            ${todo.text}
+            <button @click=${() => this._removeTodo(todo)}>X</button>
+          </li>
+        `,
+      )}
+      </ol>
+    `;
+  }
+
+  _changeTodoFinished(e, changedTodo) {
+    const eventDetails = { changedTodo, finished: e.target.checked };
+    this.dispatchEvent(new CustomEvent('change-todo-finished', { detail: eventDetails }));
+  }
+
+  _removeTodo(item) {
+    this.dispatchEvent(new CustomEvent('remove-todo', { detail:item }))
   }
 
 }
